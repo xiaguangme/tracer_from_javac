a0a9e65e274cc0f86da9579bfcb7e6eb05341146
simon_dell <chenxiaguang@gmail.com>
2013-07-11 17:48:43 +0800

    初始提交，已经含有将tracer代码注入到方法开始处

---
+++ test/org/simonme/tracer/javac/BlockTypeTest.java
@@ -0,0 +1,16 @@
+package org.simonme.tracer.javac;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BlockTypeTest
+{
+    
+    @Test
+    public void test()
+    {
+        fail("Not yet implemented");
+    }
+    
+}
---
+++ test/org/simonme/tracer/javac/Hello.java
@@ -0,0 +1,45 @@
+/*
+ * 文 件 名:  Hello.java
+ * 描    述:  <描述>
+ * 创 建 人:  Chenxiaguang
+ * 创建时间: 2013-7-11
+ * 修 改 人:  
+ * 修改时间: 
+ * 修改内容:  <修改内容>
+ */
+package org.simonme.tracer.javac;
+
+/**
+ * <一句话功能简述>
+ * <功能详细描述>
+ * 
+ * @author  Chenxiaguang
+ * @version [版本号, 2013-7-11]
+ * @see     [相关类/方法]
+ * @since   [产品/模块版本]
+ */
+public class Hello
+{
+    
+    /** 
+     * <一句话功能简述>
+     * <功能详细描述>
+     * @param args
+     * @see [类、类#方法、类#成员]
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+    public void m1(){};
+    
+    public int m2(){
+        return 1;    
+    }
+    
+    public String m3(String a1, String... a2)
+    {
+        return a1;
+    }
+}
